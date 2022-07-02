21b1211a8ce5f86ad8eb606042ceea9fa4d60900
Victor027 <humphreyvictor01@gmail.com>
2022-07-01 20:31:51 -0700

    assigning a random number to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,10 @@
 
 
 /**
-
  * main - assigns a random number to int n everytime
-
  * it executes, and prints it
-
+ *
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
@@ -21,7 +18,6 @@ int main(void)
 {
 
 	int n;
-
 
 
 	srand(time(0));
