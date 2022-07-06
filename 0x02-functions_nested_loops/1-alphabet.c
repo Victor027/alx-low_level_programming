ba9d8659fecc085f440509ca18c6a7eccb4bb3be
Victor027 <humphreyvictor01@gmail.com>
2022-07-06 00:59:24 -0700

    checking for lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,10 +15,19 @@ void print_alphabet(void)
 	while (alp <= 'z')
 	{
 		_putchar (alp);
-
 		alp++;
 	}
 
 	_putchar ('\n');
+}
+
+/**
+ * main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
