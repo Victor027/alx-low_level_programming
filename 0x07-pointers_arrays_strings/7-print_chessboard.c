57e4ec46feaa1bdea052b947cd66a7996d18cffc
Victor027 <humphreyvictor01@gmail.com>
2022-07-18 13:28:46 -0700

    even more on pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,32 +1,21 @@
 #include "main.h"
 
 /**
-* _strstr - function locate
-* @haystack: pointer to char
-* @needle: pointer to char
-* Return: 0
-*/
+ * print_chessboard - prints chessboard
+ * @a: matrix
+ */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
-	char *result = haystack, *fneedle = needle;
+	int i, j;
 
-	while (*haystack)
+	for (i = 0; i < 8; i++)
 	{
-		while (*needle)
+		for (j = 0; j < 8; j++)
 		{
-			if (*haystack++ != *needle++)
-			{
-				break;
-			}
-		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
+			_putchar(*(*(i + a) + j));
+
+
+	_putchar('\n');
 	}
-	return (0);
 }
